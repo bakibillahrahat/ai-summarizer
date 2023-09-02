25f48a3afa83f361f3eb152a06c0fb8b434b8b89
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-03 03:43:18 +0600

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -1,20 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-
-// const options = {
-//     method: 'GET',
-//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-//     params: {
-//       url: 'https://time.com/6266679/musk-ai-open-letter/',
-//       length: '3'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6',
-//       'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-//     }
-//   };
   
-
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
